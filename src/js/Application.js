4770f9a83d5eee7b266ee9e544bcf49943eda7fb
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-23 11:23:20 +0200

    Солже

--- src/js/Application.js
+++ src/js/Application.js
@@ -12,5 +12,17 @@ export default class Application extends EventEmitter {
     this.init();
     this.emit(Application.events.READY);
   }
-  init() {}
+  init() {
+    const article = document.getElementsByClassName("article");
+
+    article[0].addEventListener("click", function() {
+      anime({
+        targets: ".article p",
+        translateX: 250,
+        direction: 1500,
+        loop: true,
+        easing: 'spring(1, 80, 10, 0)'
+      });
+    });
+  }
 }
